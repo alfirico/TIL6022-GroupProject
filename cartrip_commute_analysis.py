98bdd761980a97f3f44c8aadaa69c97d57c121e0
Mjpelozo <marianajpelozo@hotmail.com>
2024-11-07 15:12:12 +0100

    Add graphs for commuting

--- cartrip_commute_analysis.py
+++ cartrip_commute_analysis.py
@@ -1,4 +1,7 @@
+'''Using the data created by the file commute_data_cleanup.py, this code will organize the data for car commuting considering the weekdays and years 
+in interest, outputing tbles and graphs for the analysis.'''
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the cleaned commuting data
 df = pd.read_csv('FinalCommuteData/total_commuting_data.csv')
@@ -21,4 +24,27 @@ df_weekdays_cartrip['Total'] = df_weekdays_cartrip.sum(axis=1) / 5
 df_weekdays_cartrip = df_weekdays_cartrip.round(3)
 df_weekdays_cartrip.columns.name = 'Average car trip/person/weekday'
 
+# Print table
 print("The average of car trips per person per weekday for 2018, 2019, 2022 and 2023 is the following: \n", df_weekdays_cartrip)
+
+# Visualization
+# Creating the DataFrame
+data = {
+    'Year': [2018, 2019, 2022, 2023],
+    'Monday': [0.353, 0.330, 0.280, 0.272],
+    'Tuesday': [0.373, 0.350, 0.295, 0.291],
+    'Wednesday': [0.339, 0.325, 0.255, 0.262],
+    'Thursday': [0.349, 0.330, 0.285, 0.282],
+    'Friday': [0.309, 0.285, 0.220, 0.220],
+}
+df_weekdays_cartrip = pd.DataFrame(data).set_index('Year')
+df_weekdays_cartrip.columns.name = ''
+
+# Plotting graph
+df_weekdays_cartrip.plot(kind='bar', grid=True, figsize=(10, 6))
+plt.xlabel('Year')
+plt.ylabel('Average Car Trip per Person per Weekday')
+plt.ylim(0, 0.8)  # Setting y-axis range from 0 to 0.8 in order to show the result clearly
+plt.title('Average Car Trips per Person per Weekday (2018, 2019, 2022, 2023)')
+plt.xticks(rotation=0)
+plt.show()
--- commute_analysis.py
+++ commute_analysis.py
@@ -1,6 +1,7 @@
 '''Using the data created by the file commute_data_cleanup.py, this code will organize the data for general commuting considering the weekdays and years 
 in interest, outputing tbles and graphs for the analysis.'''
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the cleaned commuting data
 df = pd.read_csv('FinalCommuteData/total_commuting_data.csv')
@@ -22,7 +23,30 @@ df_weekdays_trip['Total'] = df_weekdays_trip.mean(axis=1)
 # Adjust order and labels
 df_weekdays_trip.columns.name = 'Average commute trip/person/weekday'
 
-
+# Print table
 print(
     "The average of trips per person per weekday for 2018, 2019, 2022 and 2023 is the following: \n",
     df_weekdays_trip)
+
+
+#Visualization
+# Creating the DataFrame
+data = {
+    'Year': [2018, 2019, 2022, 2023],
+    'Monday': [0.72, 0.66, 0.56, 0.57],
+    'Tuesday': [0.76, 0.70, 0.59, 0.61],
+    'Wednesday': [0.69, 0.65, 0.51, 0.55],
+    'Thursday': [0.71, 0.66, 0.57, 0.59],
+    'Friday': [0.63, 0.57, 0.44, 0.46],
+}
+df_commute_trips = pd.DataFrame(data).set_index('Year')
+df_commute_trips.columns.name = ''
+
+# Plotting graph
+df_commute_trips.plot(kind='bar', grid=True, figsize=(10, 6))
+plt.xlabel('Year')
+plt.ylabel('Average Commute Trip per Person per Weekday')
+plt.ylim(0, 0.8)  # Setting y-axis range from 0 to 0.8 in order to show the result clearly
+plt.title('Average Commute Trips per Person per Weekday (2018, 2019, 2022, 2023)')
+plt.xticks(rotation=0)
+plt.show()
